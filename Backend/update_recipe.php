17e7dae0830053f0086bf738037e42fbd9b2c828
LayaneTeixeira <162758897+LayaneTeixeira@users.noreply.github.com>
2024-11-27 19:38:54 -0300

    Update_recipe.php

--- Backend/update_recipe.php
+++ Backend/update_recipe.php
@@ -1,5 +1,4 @@
 <?php
-// Conectar ao banco de dados
 $dsn = 'sqlite:../database/nutrigenius.db';
 try {
     $db = new PDO($dsn);
@@ -8,14 +7,11 @@ try {
     exit;
 }
 
-// Receber dados JSON
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Atualizar a receita
 $query = "UPDATE recipes SET name = ?, description = ?, ingredients = ?, instructions = ? WHERE id = ?";
 $stmt = $db->prepare($query);
 $stmt->execute([$data['name'], $data['description'], $data['ingredients'], $data['instructions'], $data['id']]);
 
-// Retornar a resposta
 echo json_encode(["message" => "Receita atualizada com sucesso!"]);
 ?>
