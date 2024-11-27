08e517aefe05e20ae48206d14556a357ff434965
LayaneTeixeira <162758897+LayaneTeixeira@users.noreply.github.com>
2024-11-27 19:36:55 -0300

    Create_recipe.php

--- Backend/Create_recipe.php
+++ Backend/Create_recipe.php
@@ -1,5 +1,4 @@
 <?php
-// Conectar ao banco de dados
 $dsn = 'sqlite:../database/nutrigenius.db';
 try {
     $db = new PDO($dsn);
@@ -8,14 +7,12 @@ try {
     exit;
 }
 
-// Receber dados JSON
 $data = json_decode(file_get_contents("php://input"), true);
 
-// Inserir no banco de dados
+
 $query = "INSERT INTO recipes (name, description, ingredients, instructions) VALUES (?, ?, ?, ?)";
 $stmt = $db->prepare($query);
 $stmt->execute([$data['name'], $data['description'], $data['ingredients'], $data['instructions']]);
 
-// Retornar a resposta
 echo json_encode(["message" => "Receita criada com sucesso!"]);
 ?>
